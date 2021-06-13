891d1777b3d66e930d69f319393fa9d201cbd53c
Zafer Yusuf Vardar <mymmym22@gmail.com>
2021-06-13 03:57:41 +0300

    add decompression functions

--- main.c
+++ main.c
@@ -8,13 +8,15 @@ int getPGMRows(char *fileName);
 int getPGMGrayValue(char *fileName);
 int getElementFromFile(char *fileName, int pos);
 int *getPGMData(char *fileName);
+int *getPGMDataFromCMP(char *fileName);
+void decompress(char *inFile, char *outFile);
 FILE *readFile(char *fileName);
 FILE *writeFile(char *fileName);
 
 int main()
 {
     FILE *fp, *outFp;
-    char ch, fileName[25], outName[25];
+    char ch, fileName[25], outName[25], uncompName[25];
     printf("Enter filename: ");
     gets(fileName);
 
@@ -40,6 +42,11 @@ int main()
         }
         fprintf(outFp, "%d %d ", count, *(imgArrPtr + i));
     }
+    printf("Tamamlandi. \nDecompress: ");
+    fclose(outFp);
+    gets(uncompName);
+    printf("%d rows, %d columns, %d gray", getElementFromFile(uncompName, 0), getElementFromFile(uncompName, 1), getElementFromFile(uncompName, 2));
+    decompress(uncompName, "test_decoded.pgm");
     return 0;
 }
 int getElementFromFile(char *fileName, int pos)
@@ -57,6 +64,7 @@ int getElementFromFile(char *fileName, int pos)
         if (ch != ' ' & ch != '\n')
         {
             buffer[reading] = ch;
+            printf("bf: %s\n", buffer);
             reading++;
         }
         else
@@ -147,4 +155,91 @@ int *getPGMData(char *fileName){
     }
     fclose(fp);
     return imgArrPtr;
+}
+
+int *getPGMDataFromCMP(char *fileName){
+    FILE *fp;
+    fp = readFile(fileName);
+    int reading = 0;
+    char ch;
+    int counter = 0;
+    int pixel = 0;
+    int *imgArrPtr = (int *)malloc(getElementFromFile(fileName, 0) * getElementFromFile(fileName, 1) * sizeof(int));
+    while ((ch = fgetc(fp)) != EOF)
+    {
+        char buffer[4];
+        if (ch != ' ' & ch != '\n')
+        {
+            buffer[reading] = ch;
+            reading++;
+        }
+        else
+        {
+            reading = 0;
+            counter++;
+            if (counter > 3)
+            {
+                if (!isdigit(buffer[0])){
+                    counter--;
+                }
+            }
+            memset(buffer, 0, sizeof(buffer));
+        }
+    }
+    rewind(fp);
+    int rleLength = counter - 3;
+    int *rleArrayPtr = (int *)malloc(rleLength * sizeof(int));
+    counter = 0; reading = 0;
+    while ((ch = fgetc(fp)) != EOF)
+    {
+        char buffer[4];
+        if (ch != ' ' & ch != '\n')
+        {
+            buffer[reading] = ch;
+            reading++;
+        }
+        else
+        {
+            reading = 0;
+            counter++;
+            if (counter > 3)
+            {
+                if (!isdigit(buffer[0])){
+                    counter--;
+                }
+                else{
+                    sscanf(buffer, "%d", rleArrayPtr + counter - 4);
+                }
+            }
+            memset(buffer, 0, sizeof(buffer));
+        }
+    }
+    int i, j;
+    printf("\n\n");
+    for (i = 0; i < rleLength; i = i + 2)
+    {
+        for(j = 0; j < *(rleArrayPtr + i); j++){
+            *(imgArrPtr + pixel) = *(rleArrayPtr + i + 1);
+            pixel++;
+            printf("%d ", *(rleArrayPtr + i + 1));
+        }
+    }
+    fclose(fp);
+    return imgArrPtr;
+}
+
+void decompress(char *inFile, char *outFile){
+    int rows = getElementFromFile(inFile, 0);
+    int columns = getElementFromFile(inFile, 1);
+    int gray = getElementFromFile(inFile, 2);
+    int *data = getPGMDataFromCMP(inFile);
+
+    FILE *fp = writeFile(outFile);
+    fprintf(fp, "P2 %d %d %d\n", columns, rows, gray);
+    int i;
+    for (i = 0; i < rows * columns; i++)
+    {
+        fprintf(fp, "%d ", *(data + i));
+    }
+    fclose(fp);
 }
